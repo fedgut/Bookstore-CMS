1ef2ee0cb73a00b502718aa8da765f4f08c127ac
fedgut <fedgut@gmail.com>
2020-08-19 12:56:59 -0500

    Create bookform component

---
+++ src/components/BookForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const BookForm = () => {
+  const categories = [
+    'Action',
+    'Biography',
+    'History',
+    'Horror',
+    'Kids',
+    'Learning',
+    'Sci-Fi',
+  ];
+
+  return (
+    <div>
+      <form>
+        <input type="text" name="title" id="title" placeholder="Title" />
+        <select name="category" id="category">
+          {categories.map(category => (
+            <option key={`${category}`} value={`${category}`}>
+              {`${category}`}
+            </option>
+          ))}
+        </select>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default BookForm;
