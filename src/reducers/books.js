bc73800a1ecec9cb1950e0e8c8d77ad6195becba
fedgut <fedgut@gmail.com>
2020-08-19 15:21:32 -0500

    Fix removeBook reducer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,32 +1,25 @@
-// const createBook = (state, action) => {
-//   return [...state.books,
-//   {
-//     id: action.id,
-//     title: action.title,
-//     category: action.category
-//   }];
-// }
+const createBook = (state, action) => {
+  return [
+    ...state,
+    {
+      id: action.id,
+      title: action.title,
+      category: action.category,
+    },
+  ];
+};
 
-// const removeBook = (state, action) => {
-//   return [...state.books].map(book => {
-//     if(book.id != action.id) {
-//       book;
-//     }
-//   });
-// }
+const removeBook = (state, action) => state.filter(book => book.id !== action.id);
 
-const booksReducer = (state = [], action) => {
-  // switch(action.type) {
-  //   case 'CREATE_BOOK':
-  //     return createBook(state, action);
-  //   break;
-  //   case 'REMOVE_BOOK':
-  //     return removeBook(state, action);
-  //   break;
-  //   default:
-  //     return state;
-  // }
-  return "";
-}
+const books = (state = [], action) => {
+  switch (action.type) {
+    case 'CREATE_BOOK':
+      return createBook(state, action);
+    case 'REMOVE_BOOK':
+      return removeBook(state, action);
+    default:
+      return state;
+  }
+};
 
-export default booksReducer;
+export default books;
--- src/reducers/books.test.js
+++ src/reducers/books.test.js
@@ -1,31 +1,31 @@
-import { booksReducer } from './books';
+import books from './books';
 
-const stateEmpty = []
+const stateEmpty = [];
 
-const stateFull = { 
-  books: [{
+const stateFull = [
+  {
     id: 1,
-    title: "some title",
-    category: "Sci-Fi"
-  }]
-}
+    title: 'some title',
+    category: 'Sci-Fi',
+  },
+];
 
 const actionCreate = {
   type: 'CREATE_BOOK',
   id: 1,
-  title: "some title",
-  category: "Sci-Fi"
-}
+  title: 'some title',
+  category: 'Sci-Fi',
+};
 
 const actionRemove = {
   type: 'REMOVE_BOOK',
-  id: 1
-}
+  id: 1,
+};
 
 it('should add a book', () => {
-  expect(booksReducer(stateEmpty, actionCreate)).toEqual(stateFull);
+  expect(books(stateEmpty, actionCreate)).toEqual(stateFull);
 });
 
 it('should remove a book', () => {
-  expect(booksReducer(stateFull, actionRemove)).toEqual(stateEmpty);
+  expect(books(stateFull, actionRemove)).toEqual(stateEmpty);
 });
