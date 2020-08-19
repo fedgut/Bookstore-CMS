30be1caeec113be92844efb37de5234f02e4f357
fedgut <fedgut@gmail.com>
2020-08-19 11:10:25 -0500

    Delete further unnecesarry files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div className="App"> Blank Project</div>;
+  return <div className="App"> Blank Project Basic Template</div>;
 }
 
 export default App;
