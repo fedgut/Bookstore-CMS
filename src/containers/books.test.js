b4854a4754b9304d1a0b4b3dbd6fb967c49c377d
fedgut <fedgut@gmail.com>
2020-08-25 10:32:53 -0500

    remove unnescesary file

---
+++ src/containers/books.test.js
@@ -0,0 +1,29 @@
+import books from '../reducers/books';
+
+const stateEmpty = [];
+
+const stateFull = [
+  {
+    id: 1,
+    title: 'some title',
+    category: 'Sci-Fi',
+  },
+];
+
+const actionCreate = {
+  type: 'CREATE_BOOK',
+  book: { id: 1, title: 'some title', category: 'Sci-Fi' },
+};
+
+const actionRemove = {
+  type: 'REMOVE_BOOK',
+  id: 1,
+};
+
+it('should add a book', () => {
+  expect(books(stateEmpty, actionCreate)).toEqual(stateFull);
+});
+
+it('should remove a book', () => {
+  expect(books(stateFull, actionRemove)).toEqual(stateEmpty);
+});
